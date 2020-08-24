3aa55f2b1a08991a8c0908b411b7d4b2ef82defc
Li Dan <lidan@menhoo.com>
2020-08-24 17:41:32 +0800

    兼容flutter 1.20.0

--- lib/src/date_picker_theme.dart
+++ lib/src/date_picker_theme.dart
@@ -15,12 +15,12 @@ const double DATETIME_PICKER_TITLE_HEIGHT = 44.0;
 
 /// Default value of DatePicker's column height.
 const double DATETIME_PICKER_ITEM_HEIGHT = 36.0;
-
+  
 /// Default value of DatePicker's item [TextStyle].
 const TextStyle DATETIME_PICKER_ITEM_TEXT_STYLE =
     const TextStyle(color: Color(0xFF000046), fontSize: 16.0);
 
-class DateTimePickerTheme with DiagnosticableMixin {
+class DateTimePickerTheme with Diagnosticable {
   final cancelDefault = const Text('OK');
 
   /// DateTimePicker theme.
